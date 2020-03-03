00c16322958a7e7c66a2a0963bf003d9539aa0f7
Audra T <audraturuta@gmail.com>
2020-03-03 14:53:12 -0500

    database push finished

--- src/Instructions.js
+++ src/Instructions.js
@@ -10,7 +10,7 @@ class Instructions extends Component {
                 <li>Check the box to mark the habit as done, and enter a note if you'd like.</li>
                 <li>Click submit to store it!</li>
                 <li>The days you have completed your habit will be blue, and days you have added a note have a dot. </li>
-                <li>To retrieve your data: select the day and click "get info".</li>
+                <li>Your habit logs will appear below the tracker.</li>
             </ul>
         );
     }
