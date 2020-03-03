6606c9997f6348b4291d9cbe50c9aef360f45f0c
Audra T <audraturuta@gmail.com>
2020-03-03 16:11:42 -0500

    Updates

---
+++ precache-manifest.ba78e235e084448933a4f56ebb7ca321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c613f5c8835a0568b2509f175bb4dbd6",
+    "url": "/handy-habits/index.html"
+  },
+  {
+    "revision": "bd6e76d98d2394b1609f",
+    "url": "/handy-habits/static/css/main.e4b42a8f.chunk.css"
+  },
+  {
+    "revision": "8779ebf36e0aa7ec67c8",
+    "url": "/handy-habits/static/js/2.f5ee815a.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/handy-habits/static/js/2.f5ee815a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6e76d98d2394b1609f",
+    "url": "/handy-habits/static/js/main.902dd823.chunk.js"
+  },
+  {
+    "revision": "c238a5b2f7a7f54c217c",
+    "url": "/handy-habits/static/js/runtime-main.42c3c0b7.js"
+  }
+]);
