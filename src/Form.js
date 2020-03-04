c68b687c6f8e79418a4c6a10faf31c21ec356d8e
Audra T <audraturuta@gmail.com>
2020-03-03 20:35:17 -0500

    button functionality

--- src/Form.js
+++ src/Form.js
@@ -4,9 +4,11 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
-    render() {
-        // const clearRadio = document.getElementById([`${this.props.stateFromParent.radioButton}`]).attr('checked',false);
+    pressed = (id) => {
+        document.getElementById(id).focus()
+    }
 
+    render() {
         return (
             <div>
                 <form 
@@ -21,96 +23,95 @@ class Form extends Component {
                         <legend>Day</legend>
                         <h2>Day</h2>
 
-                        <label htmlFor="1" tabIndex="1">1
-                            <input type="radio" id="1" name="day" value="1"/>
-                        </label>
+                        <input type="radio" id="1" name="day" value="1"/>
+                        <label htmlFor="1" tabIndex="1" onKeyPress={()=> {this.pressed("1")}}>1</label>
 
-                        <label htmlFor="2" tabIndex="1">2</label>
                         <input type="radio" id="2" name="day" value="2" />
+                        <label htmlFor="2" tabIndex="1" onKeyPress={()=> {this.pressed("2")}} >2</label>
 
-                        <label htmlFor="3" tabIndex="1">3</label>
                         <input type="radio" id="3" name="day" value="3" />
+                        <label htmlFor="3" tabIndex="1" onKeyPress={()=> {this.pressed("3")}}>3</label>
 
-                        <label htmlFor="4" tabIndex="1">4</label>
                         <input type="radio" id="4" name="day" value="4" />
+                        <label htmlFor="4" tabIndex="1" onKeyPress={()=> {this.pressed("4")}}>4</label>
 
-                        <label htmlFor="5" tabIndex="1">5</label>
                         <input type="radio" id="5" name="day" value="5" />
+                        <label htmlFor="5" tabIndex="1" onKeyPress={()=> {this.pressed("5")}}>5</label>
 
-                        <label htmlFor="6" tabIndex="1">6</label>
                         <input type="radio" id="6" name="day" value="6" />
+                        <label htmlFor="6" tabIndex="1" onKeyPress={()=> {this.pressed("6")}}>6</label>
 
-                        <label htmlFor="7" tabIndex="1">7</label>
                         <input type="radio" id="7" name="day" value="7" />
+                        <label htmlFor="7" tabIndex="1" onKeyPress={()=> {this.pressed("7")}}>7</label>
 
-                        <label htmlFor="8" tabIndex="1">8</label>
                         <input type="radio" id="8" name="day" value="8" />
+                        <label htmlFor="8" tabIndex="1" onKeyPress={()=> {this.pressed("8")}}>8</label>
 
-                        <label htmlFor="9" tabIndex="1">9</label>
                         <input type="radio" id="9" name="day" value="9" />
+                        <label htmlFor="9" tabIndex="1" onKeyPress={()=> {this.pressed("9")}}>9</label>
 
-                        <label htmlFor="10" tabIndex="1">10</label>
                         <input type="radio" id="10" name="day" value="10" />
+                        <label htmlFor="10" tabIndex="1" onKeyPress={()=> {this.pressed("10")}}>10</label>
 
-                        <label htmlFor="11" tabIndex="1">11</label>
                         <input type="radio" id="11" name="day" value="11" />
+                        <label htmlFor="11" tabIndex="1" onKeyPress={()=> {this.pressed("11")}}>11</label>
 
-                        <label htmlFor="12" tabIndex="1">12</label>
                         <input type="radio" id="12" name="day" value="12" />
+                        <label htmlFor="12" tabIndex="1" onKeyPress={()=> {this.pressed("12")}}>12</label>
 
-                        <label htmlFor="13" tabIndex="1">13</label>
                         <input type="radio" id="13" name="day" value="13" />
+                        <label htmlFor="13" tabIndex="1" onKeyPress={()=> {this.pressed("13")}}>13</label>
 
-                        <label htmlFor="14" tabIndex="1">14</label>
                         <input type="radio" id="14" name="day" value="14" />
+                        <label htmlFor="14" tabIndex="1" onKeyPress={()=> {this.pressed("14")}}>14</label>
 
-                        <label htmlFor="15" tabIndex="1">15</label>
                         <input type="radio" id="15" name="day" value="15" />
+                        <label htmlFor="15" tabIndex="1"onKeyPress={()=> {this.pressed("15")}}>15</label>
 
-                        <label htmlFor="16" tabIndex="1">16</label>
                         <input type="radio" id="16" name="day" value="16" />
+                        <label htmlFor="16" tabIndex="1" onKeyPress={()=> {this.pressed("16")}}>16</label>
 
-                        <label htmlFor="17" tabIndex="1">17</label>
                         <input type="radio" id="17" name="day" value="17" />
+                        <label htmlFor="17" tabIndex="1" onKeyPress={()=> {this.pressed("17")}}>17</label>
 
-                        <label htmlFor="18" tabIndex="1">18</label>
                         <input type="radio" id="18" name="day" value="18" />
+                        <label htmlFor="18" tabIndex="1" onKeyPress={()=> {this.pressed("18")}}>18</label>
 
-                        <label htmlFor="19" tabIndex="1">19</label>
                         <input type="radio" id="19" name="day" value="19" />
+                        <label htmlFor="19" tabIndex="1" onKeyPress={()=> {this.pressed("19")}}>19</label>
 
-                        <label htmlFor="20" tabIndex="1">20</label>
                         <input type="radio" id="20" name="day" value="20" />
+                        <label htmlFor="20" tabIndex="1" onKeyPress={()=> {this.pressed("20")}}>20</label>
 
-                        <label htmlFor="21" tabIndex="1">21</label>
                         <input type="radio" id="21" name="day" value="21" />
+                        <label htmlFor="21" tabIndex="1" onKeyPress={()=> {this.pressed("21")}}>21</label>
 
-                        <label htmlFor="22" tabIndex="1">22</label>
                         <input type="radio" id="22" name="day" value="22" />
+                        <label htmlFor="22" tabIndex="1" onKeyPress={()=> {this.pressed("22")}}>22</label>
 
-                        <label htmlFor="23" tabIndex="1">23</label>
                         <input type="radio" id="23" name="day" value="23" />
+                        <label htmlFor="23" tabIndex="1" onKeyPress={()=> {this.pressed("23")}}>23</label>
 
-                        <label htmlFor="24" tabIndex="1">24</label>
                         <input type="radio" id="24" name="day" value="24" />
+                        <label htmlFor="24" tabIndex="1" onKeyPress={()=> {this.pressed("24")}}>24</label>
 
-                        <label htmlFor="25" tabIndex="1">25</label>
                         <input type="radio" id="25" name="day" value="25" />
+                        <label htmlFor="25" tabIndex="1" onKeyPress={()=> {this.pressed("25")}}>25</label>
 
-                        <label htmlFor="26" tabIndex="1">26</label>
                         <input type="radio" id="26" name="day" value="26" />
+                        <label htmlFor="26" tabIndex="1" onKeyPress={()=> {this.pressed("26")}}>26</label>
 
-                        <label htmlFor="27" tabIndex="1">27</label>
                         <input type="radio" id="27" name="day" value="27"/>
+                        <label htmlFor="27" tabIndex="1" onKeyPress={()=> {this.pressed("27")}}>27</label>
 
-                        <label htmlFor="28" tabIndex="1">28</label>
                         <input type="radio" id="28" name="day" value="28" />
+                        <label htmlFor="28" tabIndex="1" onKeyPress={()=> {this.pressed("28")}}>28</label>
 
-                        <label htmlFor="29" tabIndex="1">29</label>
                         <input type="radio" id="29" name="day" value="29" />
+                        <label htmlFor="29" tabIndex="1" onKeyPress={()=> {this.pressed("29")}}>29</label>
 
-                        <label htmlFor="30" tabIndex="1">30</label>
                         <input type="radio" id="30" name="day" value="30" />
+                        <label htmlFor="30" tabIndex="1" onKeyPress={()=> {this.pressed("30")}}>30</label>
 
 
                     </fieldset>
